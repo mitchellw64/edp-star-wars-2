cf205875b5479d45685af553d3e3554a07e36af4
mitchellw64 <mitchellworthington64@gmail.com>
2025-06-23 11:21:35 -0400

    added documentation to planet.js

--- planet.js
+++ planet.js
@@ -3,10 +3,13 @@
 // const id = sp.get('id');
 
 const baseUrl = `http://localhost:9001/api`;
+
+// planet attributes, found from messing with api calls in postman
 let climate, surfaceWater, name, diameter, rotationPeriod, terrain, gravity, orbitalPeriod, population;
 
+// runs on page load
 addEventListener('DOMContentLoaded', () => {
-  // add more here
+  // finds id in html 
   nameH1 = document.querySelector('h1#name');
   populationSpan = document.querySelector('span#population');
   climateSpan = document.querySelector('span#climate');
@@ -17,14 +20,19 @@ addEventListener('DOMContentLoaded', () => {
   orbitalPeriodSpan = document.querySelector('span#orbital_period');
   rotationPeriodSpan = document.querySelector('span#rotation_period');
 
+  // going to add characters/films to <ul> list
   charactersUl = document.querySelector('#characters>ul');
   filmsUl = document.querySelector('#films>ul');
   
+  // get id from baseURL
   const sp = new URLSearchParams(window.location.search);
   const id = sp.get('id');
+
+  // collect data
   getPlanet(id);
 });
 
+// collects data from all 3 apis (planet, characters on planet, films on planet)
 async function getPlanet(id) {
     try {
         planet = await fetchPlanet(id);
@@ -36,12 +44,14 @@ async function getPlanet(id) {
     renderPlanet(planet);
 }
 
+// Sample fetch call
 // fetch('http://localhost:9000/api/socks/1/10')
 //   .then(response => response.json())
 //   .then(data => console.log('Data:', data))
 //   .catch(error => console.error('Error:', error))
 //   .finally(() => console.log('Fetch attempt completed.'));
 
+// fetches planet attributes
 async function fetchPlanet(id) {
     let planetURL = `http://localhost:9001/api/planets/${id}`;
     const planet = await fetch(planetURL)
@@ -51,6 +61,7 @@ async function fetchPlanet(id) {
     return planet;
 }
 
+// fetches characters who were on planet
 async function fetchCharacters(id) {
     let charactersURL = `http://localhost:9001/api/planets/${id}/characters`;
     const characters = await fetch(charactersURL)
@@ -60,6 +71,7 @@ async function fetchCharacters(id) {
     return characters;
 }
 
+// fetches films featuring the planet
 async function fetchFilms(id) {
     let filmsURL = `http://localhost:9001/api/planets/${id}/films`;
     const films = await fetch(filmsURL)
