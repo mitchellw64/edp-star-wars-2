2823769b16171631b76e830d67f1d9c644cdbd90
Khuzie Din <khuzi98@gmail.com>
2025-07-01 09:17:21 -0400

    added characters.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,38 +1,32 @@
 import React, { useState, useEffect } from "react";
+import Characters from './Characters';
 
 const App = () => {
-
-  const [characterData, setCharacterData] = useState();
+  const [characterData, setCharacterData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response = await fetch('http://localhost:3000/api/characters');
-            if (!response.ok) {
-                throw new Error('Data could not be fetched!');
-            }
-            const json_response = await response.json();
-            console.log(json_response);
-            setCharacterData(json_response); // assign JSON response to the data variable.
-        } catch (error) {
-            console.error('Error fetching socks:', error);
+      try {
+        const response = await fetch('http://localhost:3000/api/characters');
+        if (!response.ok) {
+          throw new Error('Data could not be fetched!');
         }
+        const json_response = await response.json();
+        setCharacterData(json_response);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
     };
 
     fetchData();
   }, []);
 
-    return (
-        <div>
-            <h1>This is the Sock component!</h1>
-            {/* {
-              // Change from static sock_data to data coming from sock API
-              characterData.map((sock) => (
-                  <Sock key={sock._id} data={sock} /> // Change id to _id. _id is the key in the API response
-              ))
-            } */}
-        </div>
-    );
+  return (
+    <div>
+      <h1>Star Wars Characters</h1>
+      <Characters characters={characterData} />
+    </div>
+  );
 };
 
 export default App;
